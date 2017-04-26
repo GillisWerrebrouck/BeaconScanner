5644cbe92d461b73209dc02f092749bd1fd370f5
GillisWerrebrouck <gillis-wer@hotmail.be>
2017-04-26 18:14:39 +0000

    Fine tuning the scanning for beacons

--- scan.py
+++ scan.py
@@ -11,6 +11,15 @@ from socket import (
     SOL_HCI,
     HCI_FILTER,
 )
+import RPi.GPIO as GPIO
+import time, threading
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18, GPIO.OUT)
+
+os.system("sudo hciconfig hci0 down")
+os.system("sudo hciconfig hci0 up")
 
 if not os.geteuid() == 0:
     sys.exit("script only works as root")
@@ -30,8 +39,8 @@ sock.bind((dev_id,))
 
 err = bluez.hci_le_set_scan_parameters(sock.fileno(), 0, 0x10, 0x10, 0, 0, 1000);
 if err < 0:
-    raise Exception("Set scan parameters failed")
     # occurs when scanning is still enabled from previous call
+    raise Exception("Set scan parameters failed")
 
 # allows LE advertising events
 hci_filter = struct.pack(
@@ -46,24 +55,53 @@ err = bluez.hci_le_set_scan_enable(
     sock.fileno(),
     1,  # 1 - turn on;  0 - turn off
     0, # 0-filtering disabled, 1-filter out duplicates
-    1000  # timeout
+    5000  # timeout
 )
 if err < 0:
+    GPIO.output(18,False)
     errnum = get_errno()
     raise Exception("{} {}".format(
         errno.errorcode[errnum],
         os.strerror(errnum)
     ))
 
-while True:
-    data = sock.recv(10240)
-    # print bluetooth address from LE Advert. packet
-    print(':'.join("{0:02x}".format(x) for x in data[12:6:-1]))
-    # print(str(data,'utf-8'))
-    print(data)
+with open("/home/pi/Documents/BeaconScanner/beacons") as f:
+    beacons = f.read().splitlines()
 
-    print((data[36]<<8)+data[37])
-    print((data[38]<<8)+data[39])
-    print(data[41])
-    print('-----')
-    # print(data.decode('utf-8', "replace"))
+devices = []
+
+def beacontimer():
+    global devices
+    isBeacon = False
+    try:
+        if not devices:
+            print("\033[91m" + "no devices" + "\033[0m")
+            GPIO.output(18,False)
+        else:
+            for d in list(set(devices)):
+                if d in beacons:
+                    print("\033[92m" + d + "\033[0m")
+                    isBeacon = True
+                else:
+                    print(d)
+
+            if isBeacon:
+                GPIO.output(18,True)
+            else:
+                GPIO.output(18,False)
+                    
+            devices = []
+    except:
+        GPIO.output(18,False)
+
+    print("----------")
+    
+    threading.Timer(2, beacontimer).start()
+
+beacontimer()
+
+while True:
+    data = sock.recv(1024)
+    # get bluetooth address from LE Advert packet
+    ba = ':'.join("{0:02x}".format(x) for x in data[12:6:-1])
+    devices.append(ba)
